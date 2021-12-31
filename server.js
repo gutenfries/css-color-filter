c357002c07d5ccce53d01a9eabb8891101738af5
Mark Gutenberger <Mark-Gutenberger@outlook.com>
2021-12-31 13:20:49 -0600

    update to yarn

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+
 const port = process.env.PORT || 2020
-app.listen(port, console.log(`app up on port ${port}`))
+
 app.use(express.static(`${__dirname}`))
+
+app.listen(port, console.log(`app up on port ${port}`))
